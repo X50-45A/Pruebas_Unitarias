c65adc6b30478a6e2aba149d73043ea9540a55ad
drago <dragosbelu2003@gmail.com>
2024-12-10 12:38:24 +0100

    Implemented PMVVehicle

--- src/micromobility/PMVehicle.java
+++ src/micromobility/PMVehicle.java
@@ -6,13 +6,36 @@ import data.GeographicPoint;
  * Internal classes involved in in the use of the service
  */
 public class PMVehicle {
- ??? // The class members
+    private final int vehicleId;
     PMVState state;
- ??? // The constructor/s
+    GeographicPoint position;
+    private int charge;
+    public PMVehicle(int vehicleId, GeographicPoint position, PMVState state, int charge) {
+        this.vehicleId = vehicleId;
+        this.charge = charge;
+        this.position = position;
+        this.state = state;
+    }
+    // The constructor/s
     // All the getter methods
     // The setter methods to be used are only the following ones
-    public void setNotAvailb () { . . . }
-    public void setUnderWay () { . . . }
-    public void setAvailb () { . . . }
-    public void setLocation (GeographicPoint gP) { . . . }
+    public void setNotAvailb () {
+        this.state = PMVState.NotAvailable;
+    }
+    public void setUnderWay () {
+        this.state = PMVState.UnderWay;
+    }
+    public void setAvailb () {
+        this.state = PMVState.Availbale;
+    }
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public int getCharge() {
+        return charge;
+    }
+    public GeographicPoint getPosition() {
+        return position;
+    }
 }
--- src/services/smartfeatures/QRDecoder.java
+++ src/services/smartfeatures/QRDecoder.java
@@ -1,6 +1,10 @@
 package services.smartfeatures;
 
+import micromobility.PMVehicle;
+
+import java.awt.image.BufferedImage;
+
 public interface QRDecoder { // Decodes QR codes from an image
-    VehicleID getVehicleID(BufferedImage QRImg) throws CorruptedImgException;
+    PMVehicle getVehicleID(BufferedImage QRImg) throws CorruptedImgException;
 }
 
