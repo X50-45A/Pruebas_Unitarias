92d3f2dd431d45e6118b8c683eff1847217585ff
BoiKisser776-2 <dtl3.udl@gmail.com>
2024-12-17 09:54:18 +0100

    Añadir tests
    eur

---
+++ Pruebas_Unitarias-main/src/services/smartfeatures/ArduinoMicroController.java
@@ -0,0 +1,11 @@
+package services.smartfeatures;
+
+public interface ArduinoMicroController { // Software for microcontrollers
+    public void setBTconnection () throws ConnectException;
+    public void startDriving () throws PMVPhisicalException, ConnectException,
+            ProceduralException;
+    public void stopDriving () throws PMVPhisicalException, ConnectException,
+            ProceduralException;
+    public void undoBTconnection ();
+}
+
