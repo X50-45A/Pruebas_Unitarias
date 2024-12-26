d35254a39b690edd3c0ed85577bfa0c7b3c0615c
BoiKisser776-2 <dtl3.udl@gmail.com>
2024-12-26 18:10:31 +0000

    User Account test problem solved and implemented vehicleIDTest waiting for implementation and testing

--- Tests/data/UserAccountTest.java
+++ Tests/data/UserAccountTest.java
@@ -6,16 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserAccountTest {
-    static UserAccount id1;
-    static UserAccount id2;
-    static UserAccount id3;
+    static UserAccount user1;
+    static UserAccount user2;
+    static UserAccount user3;
 
 
     @BeforeAll
     static void initId(){
-        id1 = new UserAccount("user1@gmail.com");
-        id2 = new UserAccount("user2@hotmail.es");
-        id3 = new UserAccount("user3@outlook.org");
+        user1 = new UserAccount("user1@gmail.com");
+        user2 = new UserAccount("user2@hotmail.es");
+        user3 = new UserAccount("user3@outlook.org");
     }
     @Test
     void nullId(){
@@ -24,32 +24,32 @@ public class UserAccountTest {
 
     @Test
     void invalidFormat(){
-        assertThrows(IllegalArgumentException.class, () -> {new StationID("user");});
-        assertThrows(IllegalArgumentException.class, () -> {new StationID("@hotmail.com");});
-        assertThrows(IllegalArgumentException.class, () -> {new StationID("user.com");});
-        assertThrows(IllegalArgumentException.class, () -> {new StationID("user@domain");});
-        assertDoesNotThrow(() -> {new StationID("user@domain.org");});
+        assertThrows(IllegalArgumentException.class, () -> {new UserAccount("user");});
+        assertThrows(IllegalArgumentException.class, () -> {new UserAccount("@hotmail.com");});
+        assertThrows(IllegalArgumentException.class, () -> {new UserAccount("user.com");});
+        assertThrows(IllegalArgumentException.class, () -> {new UserAccount("user@domain");});
+        assertDoesNotThrow(() -> {new UserAccount("user@domain.org");});
     }
 
     @Test
     void equals(){
-        assertEquals(id1, new StationID("user1@gmail.com"));
-        assertNotEquals(id1,id2);
-        assertNotEquals(id1,id3);
-        assertNotEquals(id2,id3);
+        assertEquals(user1, new UserAccount("user1@gmail.com"));
+        assertNotEquals(user1, user2);
+        assertNotEquals(user1, user3);
+        assertNotEquals(user2, user3);
     }
 
     @Test
     void hashcode(){
-        assertEquals(id1.hashCode(), "user1@gmail.com".hashCode());
-        assertEquals(id2.hashCode(), "user2@hotmail.es".hashCode());
-        assertEquals(id3.hashCode(), "user3@outlook.org".hashCode());
+        assertEquals(user1.hashCode(), "user1@gmail.com".hashCode());
+        assertEquals(user2.hashCode(), "user2@hotmail.es".hashCode());
+        assertEquals(user3.hashCode(), "user3@outlook.org".hashCode());
     }
 
     @Test
     void toStringTest(){
-        assertEquals("UserAccount{" + "email=user1@gmail.com'" + '\'' + '}', id1.toString());
-        assertEquals("UserAccount{" + "email=user2@hotmail.es'" + '\'' + '}', id2.toString());
-        assertEquals("UserAccount{" + "email=user3@outlook.org'" + '\'' + '}', id3.toString());
+        assertEquals("UserAccount{" + "email=user1@gmail.com'" + '\'' + '}', user1.toString());
+        assertEquals("UserAccount{" + "email=user2@hotmail.es'" + '\'' + '}', user2.toString());
+        assertEquals("UserAccount{" + "email=user3@outlook.org'" + '\'' + '}', user3.toString());
     }
 }
--- Tests/data/VehicleIDTest.java
+++ Tests/data/VehicleIDTest.java
@@ -1,4 +1,54 @@
 package data;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VehicleIDTest {
+    static VehicleID id1;
+    static VehicleID id2;
+    static VehicleID id3;
+
+
+    @BeforeAll
+    static void initId(){
+        id1 = new VehicleID("PMV00001");
+        id2 = new VehicleID("PMV00002");
+        id3 = new VehicleID("PMV00003");
+    }
+    @Test
+    void nullId(){
+        assertThrows(NullPointerException.class, () -> {new VehicleID(null);});
+    }
+
+    @Test
+    void invalidFormat(){
+        assertThrows(IllegalArgumentException.class, () -> {new VehicleID("00000000");});
+        assertThrows(IllegalArgumentException.class, () -> {new VehicleID("@MV99999");});
+        assertThrows(IllegalArgumentException.class, () -> {new VehicleID("PMVAAAAA");});
+        assertDoesNotThrow(() -> {new VehicleID("PMV12345");});
+    }
+
+    @Test
+    void equals(){
+        assertEquals(id1, new VehicleID("PMV00001"));
+        assertNotEquals(id1,id2);
+        assertNotEquals(id1,id3);
+        assertNotEquals(id2,id3);
+    }
+
+    @Test
+    void hashcode(){
+        assertEquals(id1.hashCode(), "PMV00001".hashCode());
+        assertEquals(id2.hashCode(), "PMV00002".hashCode());
+        assertEquals(id3.hashCode(), "PMV00003".hashCode());
+    }
+
+    @Test
+    void toStringTest(){
+        assertEquals("VehicleID{" + "value=PMV00001" + '\'' + '}', id1.toString());
+        assertEquals("VehicleID{" + "value=PMV00002" + '\'' + '}', id2.toString());
+        assertEquals("VehicleID{" + "value=PMV00003" + '\'' + '}', id3.toString());
+    }
 }
