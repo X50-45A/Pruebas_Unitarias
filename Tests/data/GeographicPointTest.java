8219e80e2929ae0745ad97fca9635f456b52cbe2
drago <dragosbelu2003@gmail.com>
2024-12-26 16:08:24 +0100

    Done changes and bug fixxing

--- Tests/data/GeographicPointTest.java
+++ Tests/data/GeographicPointTest.java
@@ -1,6 +1,5 @@
 package data;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
