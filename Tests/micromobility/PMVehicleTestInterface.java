5a8fc3eaf25c73d8788a49a450a780ef4b44a82a
Albert <alsorribes@gmail.com>
2024-12-29 16:56:08 +0100

    Interface de la clase test PMVehicle

---
+++ Tests/micromobility/PMVehicleTestInterface.java
@@ -0,0 +1,13 @@
+package micromobility;
+
+import org.junit.jupiter.api.Test;
+
+public interface PMVehicleTestInterface {
+
+    /**
+     * Test for setter and getter methods of PMVehicle.
+     */
+    @Test
+    void setterAndGetter();
+
+}
